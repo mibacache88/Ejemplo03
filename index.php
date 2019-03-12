d61c8e513391f23f7c87fbeccc22d1fad4cd4ab8
cetecom <cetecom@LCV10827>
2019-03-11 22:43:31 -0300

    tercer proyecto ,  correcciones2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Ejemplo03</title>
+        <link href="estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="head">Cabecera</div>
+        <div id="menu">Menu</div>
+        
+        <div id="contenedor">Contenedor</div>
+    </body>
+</html>
